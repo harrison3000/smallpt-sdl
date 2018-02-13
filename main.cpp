0c8c7877978aebb2c18dbbb7f75486b18f17ff9f
harrison3000 <harrisonf100@gmail.com>
2018-02-13 14:07:30 -0300

    re add progress message

--- main.cpp
+++ main.cpp
@@ -188,7 +188,7 @@ int main(int argc, char *argv[]) {
 				exit(1);
 			}
 		}
-		//fprintf(stderr,"\rRendering (%d spp) %5.2f%%",samps*4,100.*y/(h-1));
+		fprintf(stderr,"\rRendering (%d spp) %5.2f%%",samps*4,100.*y/(h-1));
 		for (unsigned short x = 0, Xi[3] = {0, 0, y * y * y}; x < w; x++) { // Loop cols
 			for (int sy = 0, i = (h - y - 1) * w + x; sy < 2; sy++) { // 2x2 subpixel rows
 				for (int sx = 0; sx < 2; sx++, r = Vec()) { // 2x2 subpixel cols
